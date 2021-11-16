a82d4b47f546225ef4803490c59b061705fb182c
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-16 12:55:10 +0100

    Update index.6701a6e1.js

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -23041,7 +23041,7 @@ class MovieView extends _reactDefault.default.Component {
                     __self: this,
                     children: /*#__PURE__*/ _jsxRuntime.jsx("img", {
                         src: movie.ImagePath,
-                        crossOrigin: "anonymous",
+                        crossOrigins: "anonymous",
                         __source: {
                             fileName: "src/components/movie-view/movie-view.jsx",
                             lineNumber: 12
