4b2d9eccf871f3575c37cd58492310dcf2609a17
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-16 16:33:32 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -24,6 +24,8 @@ export class MainView extends React.Component {
                 console.log(error);
             });
     }
+    /* When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie */
+
     setSelectedMovie(newSelectedMovie) {
         this.setState({
             selectedMovie: newSelectedMovie
