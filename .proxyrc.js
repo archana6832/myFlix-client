305f2b64ff3b7b2e34157f7c5a215e25c5d28ec2
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-17 16:18:13 +0100

    Update .proxyrc.js

--- .proxyrc.js
+++ .proxyrc.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.use((req, res, next) => {
-        res.removeHeader('Cross-Origin-Resourse-Policy');
+        res.removeHeader('Cross-Origin-Resource-Policy');
         res.removeHeader('Cross-Origin-Embedder-Policy');
         next();
     });
