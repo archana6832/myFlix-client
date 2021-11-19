a1f3c51116b6031b3584dd8e2b3e7a2b1e999024
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-19 19:41:38 +0100

    Update registration-view.jsx
    React bootstrap Form added

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './registration-view.scss';
+import { Form, Button, Card, CardGroup, Col, Row } from 'react-bootstrap';
 
 export function RegistrationView(props) {
     const [username, setUsername] = useState('');
@@ -16,35 +18,75 @@ export function RegistrationView(props) {
 
     };
 
-    return (<form>
-        <label>
-            Username:
-            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-        </label>
-        <label>
-            Password:
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <label>
-            Email:
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-        </label>
-        <label>
-            Birthday:
-            <input type="birthday" value={birthday} onChange={e => setBirthday(e.target.value)} />
-        </label>
+    return (
+        <Row>
+            <Col>
+                <CardGroup>
+                    <Card>
+                        <Card.Header>Please register</Card.Header>
+                        <Card.Body>
+                            <Form>
+                                <Form.Group >
+                                    <Form.Label>Username:</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        value={username}
+                                        onChange={e => setUsername(e.target.value)}
+                                        required
+                                        placeholder='Enter a username'
+                                    />
+                                </Form.Group>
 
-        <button type="submit" onClick={handleSubmit}>Register</button>
-    </form>
+                                <Form.Group >
+                                    <Form.Label>Password:</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        value={password}
+                                        onChange={e => setPassword(e.target.value)}
+                                        required
+                                        minLength='8'
+                                        placeholder='Password'
+                                    />
+                                </Form.Group>
+
+                                <Form.Group >
+                                    <Form.Label>Email:</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        value={email}
+                                        onChange={e => setEmail(e.target.value)}
+                                        required
+                                        placeholder='Enter your Email address'
+                                    />
+                                </Form.Group>
+
+                                <Form.Group >
+                                    <Form.Label>Birthday:</Form.Label>
+                                    <Form.Control
+                                        type="birthday"
+                                        onChange={e => setBirthday(e.target.value)}
+                                        placeholder='Enter your Birthday'
+                                    />
+                                </Form.Group>
+
+                                <Button variant="primary" type="submit"
+                                    onClick={handleSubmit}>
+                                    Register
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Col>
+        </Row>
+
     );
 }
 
 
 RegistrationView.propTypes = {
-    username: PropTypes.string,
-    email: PropTypes.string,
-    birthday: PropTypes.string,
-    password: PropTypes.string,
+    onRegistration: PropTypes.func.isRequired
+};
 
-}
-export default RegistrationView;
+
+
