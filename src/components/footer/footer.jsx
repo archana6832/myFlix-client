debb7b70e08d67b2a28b9e367655f195c10c3e18
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-23 13:54:03 +0100

    Delete footer.jsx
    Removed footer.jsx

--- src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-    <div className="footer">
-        <p> Copyright 2021- MyFlix . All rights reserved</p>
-    </div>
-);
-
-export default Footer;
