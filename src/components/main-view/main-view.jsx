7a81450bb2d2f0d46115f370fd40c3c94f938781
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-30 15:40:42 +0100

    Update main-view.jsx
    Added new task requirements

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'; // React Bootstrap
 import './main-view.scss'; //import .scss
 
@@ -7,6 +8,9 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
 
 
 export class MainView extends React.Component {
@@ -21,18 +25,29 @@ export class MainView extends React.Component {
             registered: null
         };
     }
-
-    //code below integrates with the API information hosted by heroku (linked to MongoDB Atlas)
-    componentDidMount() {
-        axios.get('https://myflix-moviesapp.herokuapp.com/movies')
+    getMovies(token) {
+        axios.get('https://myflix-moviesapp.herokuapp.com/movies', {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then(response => {
+                // Assign the result to the state
                 this.setState({
                     movies: response.data
                 });
             })
-            .catch(error => {
+            .catch(function (error) {
                 console.log(error);
             });
+    }
+    //code below integrates with the API information hosted by heroku (linked to MongoDB Atlas)
+    componentDidMount() {
+        let accessToken = localStorage.getItem('token');
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
+            });
+            this.getMovies(accessToken);
+        }
     }
 
     //below code invokes a function which updates the state of selectedMovie to the specific movie 
@@ -52,13 +67,23 @@ export class MainView extends React.Component {
 
     /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
-    onLoggedIn(user) {
+    onLoggedIn(authData) {
+        console.log(authData);
         this.setState({
-            user
+            user: authData.user.Username
+        });
+
+        localStorage.setItem('token', authData.token);
+        localStorage.setItem('user', authData.user.Username);
+        this.getMovies(authData.token);
+    }
+    onLoggedOut() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.setState({
+            user: null
         });
     }
-
-
 
     render() {
         const { movies, selectedMovie, user, registered } = this.state;
@@ -84,13 +109,13 @@ export class MainView extends React.Component {
                                 <Nav className="me-auto">
                                     <Nav.Link href="#home">Movies</Nav.Link>
                                     <Nav.Link href="#user">Profile</Nav.Link>
-                                    <Nav.Link href="#logout">Logout</Nav.Link>
+                                    <button onClick={() => { this.onLoggedOut() }}>Logout</button>
                                 </Nav>
                             </Container>
                         </Navbar>
 
 
-                        <Row className="main-view justify-content-md-center">
+                        <Row className="justify-content-lg-center">
                             {selectedMovie
                                 ? (
                                     <Col md={8}>
