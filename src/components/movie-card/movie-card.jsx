cea352e5e51ea1eec36004ed3bc4587f02f54a5b
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-30 20:31:56 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
-import axios from 'axios';
 import './movie-card.scss';
-import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie } = this.props;
 
         return (
-
             <Card>
                 <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous" />
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                     <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                    <Link to={`/movies/${movie._id}`}>
+                        <Button variant="link">Open</Button>
+                    </Link>
                 </Card.Body>
             </Card>
-
         );
     }
 }
