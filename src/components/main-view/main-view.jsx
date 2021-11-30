1f6561a1ed8c2e8260b3b39765c8577364785234
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-11-30 20:31:58 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'; // React Bootstrap
 import './main-view.scss'; //import .scss
 
@@ -50,20 +50,20 @@ export class MainView extends React.Component {
         }
     }
 
-    //below code invokes a function which updates the state of selectedMovie to the specific movie 
+    /* //below code invokes a function which updates the state of selectedMovie to the specific movie 
     setSelectedMovie(movie) {
         this.setState({
             selectedMovie: movie
         });
-    }
+    }*/
 
-    //the function updates the user property to the specific user
-
-    onRegistration(registered) {
-        this.setState({
-            registered
-        });
-    }
+    /* //the function updates the user property to the specific user
+    
+        onRegistration(registered) {
+            this.setState({
+                registered
+            });
+        } */
 
     /* When a user successfully logs in, this function updates the `user` property in state to that *particular user */
 
@@ -87,58 +87,60 @@ export class MainView extends React.Component {
     }
 
     render() {
-        const { movies, selectedMovie, user, registered } = this.state;
+        const { movies, user } = this.state;
 
-        //If there is no registered user the registration view is rendered.  If a user registers in registration details are passed as a prop to RegistrationView
-        if (!registered) return <RegistrationView onRegistration={registered => this.onRegistration(registered)} />;
-        //If there is no identified user then the login view is rendered and the user details are passed as a prop to LoginView
-        if (!user)
-            return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+        return (
+
+            <Container>
+                <Router>
+                    <Navbar variant="dark" expand="lg" className="mainNavbar">
+                        <Container>
+                            <Navbar.Brand href="#home">myFlix</Navbar.Brand>
+                            <Nav className="me-auto">
+                                <Nav.Link href="#home">Movies</Nav.Link>
+                                <Nav.Link href="#user">Profile</Nav.Link>
+                                <Nav.Link onClick={() => { this.onLoggedOut() }}>Logout</Nav.Link>
+                            </Nav>
+                        </Container>
+                    </Navbar>
+
+                    <Row className="main-view justify-content-md-center">
+                        <Route exact path="/" render={() => {
+                            if (!user) return <Col>
+                                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                            </Col>
+                            if (user) return <Col>
+                                <RealProfileView />
+                            </Col>
+                        }} />
+                        <Route exact path="/register" render={() => {
+                            if (user) return <Redirect to="/" />
+                            return <Col>
+                                <RegistrationView />
+                            </Col>
+                        }} />
+
+                        <Route path="/movies/:movieId" render={({ match, history }) => {
+                            if (!user) return <Col>
+                                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                            </Col>
+                            if (movies.length === 0) return <div className="main-view" />;
+                            return <Col md={8}>
+                                <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+                            </Col>
+                        }} />
+
+                    </Row>
+
+                </Router>
+
+            </Container>
 
 
 
-        if (movies.length === 0) {
-            return <div className="main-view" />;
-
-        } else {
-            return (
-                <div className="main-view">
-                    <Container>
-                        <Navbar variant="dark" expand="lg" className="mainNavbar">
-                            <Container>
-                                <Navbar.Brand href="#home">myFlix</Navbar.Brand>
-                                <Nav className="me-auto">
-                                    <Nav.Link href="#home">Movies</Nav.Link>
-                                    <Nav.Link href="#user">Profile</Nav.Link>
-                                    <Nav.Link onClick={() => { this.onLoggedOut() }}>Logout</Nav.Link>
-                                </Nav>
-                            </Container>
-                        </Navbar>
-
-
-                        <Row className="justify-content-lg-center">
-                            {selectedMovie
-                                ? (
-                                    <Col md={8}>
-                                        <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                                    </Col>
-                                )
-                                : movies.map(movie => (
-                                    <Col md={3}>
-                                        <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                                    </Col>
-                                ))
-                            }
-                        </Row>
-
-                    </Container>
-
-                </div>
-
-            );
-        }
+        );
     }
 }
 
 
-export default MainView;
+
