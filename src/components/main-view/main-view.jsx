045fdf90c1d4a5e64641fb1596350ee19f8a6e35
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-01 19:18:08 +0100

    Update main-view.jsx
    Added Route to Genre and Director view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -118,6 +118,24 @@ export class MainView extends React.Component {
                             </Col>
 
                         }} />
+                        <Route path="/directors/:name" render={({ match, history }) => {
+                            if (!user) return <Col>
+                                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                            </Col>
+                            if (movies.length === 0) return <div className="main-view" />;
+                            return <Col md={8}>
+                                <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackClick={() => history.goBack()} />
+                            </Col>
+                        }} />
+                        <Route path="/genres/:name" render={({ match, history }) => {
+                            if (!user) return <Col>
+                                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+                            </Col>
+                            if (movies.length === 0) return <div className="main-view" />;
+                            return <Col md={8}>
+                                <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackClick={() => history.goBack()} />
+                            </Col>
+                        }} />
 
                     </Row>
                 </Router>
