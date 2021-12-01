2f10ffa9604e03ba4a6238960d02fe011bd8857d
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-01 16:19:54 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import { MainView } from "./components/main-view/main-view";
-import { MainView } from "./components/main-view/main-view";
 import Container from 'react-bootstrap/Container';
-import './index.scss';
-
-// Import statement to indicate that you need to bundle `./index.scss`
+import { MainView } from './components/main-view/main-view';
 import './index.scss';
 
 // Main component (will eventually use all the others)
