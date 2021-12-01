5bb12b44a73d16a13ce24ac9dde6986392cd6c96
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-01 11:11:52 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './movie-view.scss';
+import { Link } from "react-router-dom";
 
 export class MovieView extends React.Component {
 
