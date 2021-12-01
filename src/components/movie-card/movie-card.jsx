e0ce4ad06db56ef7502c4d3efdb71573213aea8e
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-01 16:23:41 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import './movie-card.scss';
+import { Link } from 'react-router-dom';
 
 export class MovieCard extends React.Component {
     render() {
