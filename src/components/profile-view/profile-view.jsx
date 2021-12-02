85f42d5d2550928537a7c68df5b49789ce49b8fd
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-02 11:57:19 +0100

    Update profile-view.jsx

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import axios from "axios";
+import { Form, Button, Row, Col, Card, Container, } from "react-bootstrap";
+import { setState } from 'react';
+import { PropTypes } from 'prop-types';
+import { Link } from "react-router-dom";
+import './profile-view.scss';
+
