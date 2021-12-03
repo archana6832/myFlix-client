37f2a1aef6bab2ca9850f5e5ad975c41f10ef572
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-03 12:14:44 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,3 +6,86 @@ import { PropTypes } from 'prop-types';
 import { Link } from "react-router-dom";
 import './profile-view.scss';
 
+export class ProfileView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            Username: null,
+            Password: null,
+            Email: null,
+            Birthday: null,
+            FavoriteMovies: [],
+        };
+    }
+
+    componentDidMount() {
+        let accessToken = localStorage.getItem("token");
+        if (accessToken !== null) {
+            this.setState({
+                user: localStorage.getItem('user')
+            });
+            this.getUser(accessToken);
+        }
+    }
+
+    getUser(token) {
+        const username = localStorage.getItem("user");
+        axios
+            .get(`https://myflix-moviesapp.herokuapp.com/users/`,
+                {
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            )
+            .then((response) => {
+                this.setState({
+                    userObject: response.data
+
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+
+    render() {
+        const { user, onBackClick } = this.props;
+        console.log(user);
+
+        return (
+            <Container>
+                <Card className="profile-view">
+                    <Card.Header>
+                        <div className="userdetails">
+                            <span className="label">User Details: </span>
+                            <span className="value">{this.state.user}</span>
+                        </div>
+                    </Card.Header>
+                    <Card.Body>
+                        <div className="username">
+                            <span className="label">Username: </span>
+                            <span className="value">{user.Username}</span>
+                        </div>
+                        <div className="Password">
+                            <span className="label">Password: </span>
+                            <span className="value">******</span>
+                        </div>
+                        <div className="Email">
+                            <span className="label">Email: </span>
+                            <span className="value">{user.Email}</span>
+                        </div>
+                        <div className="Birthday">
+                            <span className="label">Birthday: </span>
+                            <span className="value">{user.Birthday}</span>
+                        </div>
+
+                        <Button className="buttons" variant="danger" onClick={() => { onBackClick(null); }}>Back</Button>
+
+                    </Card.Body>
+                </Card>
+            </Container>
+
+        );
+    }
+}
