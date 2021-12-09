8fb718b1d7375561fbf23b8f4b95d4e4bd079795
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-09 19:08:49 +0100

    Update update-view.jsx
    Form validation added

--- src/components/update-view/update-view.jsx
+++ src/components/update-view/update-view.jsx
@@ -12,33 +12,44 @@ export function UpdateView(user) {
     const [email, setEmail] = useState('');
     const [birthday, setBirthday] = useState('');
 
+    //form validation
+    const [usernameError, setUsernameError] = useState({});
+    const [passwordError, setPasswordError] = useState({});
+    const [emailError, setEmailError] = useState({});
+    const [birthdateError, setBirthdateError] = useState({});
+
+
     // get from local storage
     const username = localStorage.getItem('user');
     const token = localStorage.getItem('token');
 
     const handleUpdate = (e) => {
         e.preventDefault();
+        let setisValid = formValidation();
+        if (setisValid) {
+            //Update user
+            console.log(user);
+            axios.put(`https://myflix-moviesapp.herokuapp.com/users/${username}`, {
+                Username: username,
+                Password: password,
+                Email: email,
+                Birthday: birthday
 
-        console.log(user);
-        axios.put(`https://myflix-moviesapp.herokuapp.com/users/${username}`, {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birthday: birthday
-
-        }, {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then(response => {
-                const data = response.data;
-                console.log(data);
-                alert('user has been updated');
-
+            }, {
+                headers: { Authorization: `Bearer ${token}` }
             })
-            .catch(e => {
-                console.log('error updating the user')
-            });
-    };
+                .then(response => {
+                    const data = response.data;
+                    console.log(data);
+                    alert('user has been updated');
+
+                })
+                .catch(e => {
+                    alert('Error')
+                    console.log('error updating the user')
+                });
+        };
+    }
 
     const handleDeregister = () => {
         const token = localStorage.getItem('token');
@@ -56,29 +67,95 @@ export function UpdateView(user) {
                 console.log(err, 'Deregistration did not succeed');
             })
     }
+    //Form Validation
 
+    const formValidation = () => {
+        let usernameError = {};
+        let passwordError = {};
+        let emailError = {};
+        let birthdateError = {};
+        let isValid = true;
+
+        if (username === '') {
+            usernameError.usernameEmpty = alert("Please enter a valid username.");
+            isValid = false;
+        }
+
+        if (username.trim().length < 5) {
+            usernameError.usernameShort = alert("Username needs to be at least 5 characters long.");
+            isValid = false;
+        }
+
+
+        if (password.trim().length < 8) {
+            passwordError.passwordShort = alert("Password needs to be at least 8 characters long.");
+            isValid = false;
+        }
+
+        if (!(email && email.includes(".") && email.includes("@"))) {
+            emailError.emailNotEmail = alert("Please enter correct email address.");
+            isValid = false;
+        }
+        if (birthday === '') {
+            birthdateError.birthdateEmpty = alert("Please enter your birthday.");
+            isValid = false;
+        }
+        setUsernameError(usernameError);
+        setPasswordError(passwordError);
+        setEmailError(emailError);
+        setBirthdateError(birthdateError);
+        return isValid;
+    };
 
     return (
-        <Form className='registrationForm'>
+        <Form className='updateForm'>
             <Form.Group controlId="formUsername">
                 <Form.Label>Username: </Form.Label>
                 <Form.Control type="text" required
                     onChange={e => setUsername(e.target.value)} />
+                {Object.keys(usernameError).map((key) => {
+                    return (
+                        <div key={key}>
+                            {usernameError[key]}
+                        </div>
+                    );
+                })}
             </Form.Group>
 
             <Form.Group controlId="pasword">
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" required onChange={e => setPassword(e.target.value)} />
+                {Object.keys(passwordError).map((key) => {
+                    return (
+                        <div key={key}>
+                            {passwordError[key]}
+                        </div>
+                    );
+                })}
             </Form.Group>
 
             <Form.Group controlId="email">
                 <Form.Label>Email: </Form.Label>
                 <Form.Control type="email" required onChange={e => setEmail(e.target.value)} />
+                {Object.keys(emailError).map((key) => {
+                    return (
+                        <div key={key}>
+                            {emailError[key]}
+                        </div>
+                    );
+                })}
             </Form.Group>
 
             <Form.Group controlId="birthday">
                 <Form.Label>Birthday:</Form.Label>
                 <Form.Control type="birthday" required onChange={e => setBirthday(e.target.value)} />
+                {Object.keys(birthdateError).map((key) => {
+                    return (
+                        <div key={key}>
+                            {birthdateError[key]}
+                        </div>
+                    );
+                })}
             </Form.Group>
             <Link to={`/profile`}>
                 <Button variant="secondary" type="button">Back</Button>
