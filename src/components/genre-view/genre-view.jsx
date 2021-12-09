ff49c7e9e5ff00be614f5d73cacbe34e02343291
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-09 18:34:38 +0100

    Update genre-view.jsx

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -29,3 +29,10 @@ export class GenreView extends React.Component {
         );
     }
 }
+
+GenreView.propTypes = {
+    genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+    }).isRequired
+}
