7fd8d7595cefb6f7f61b09c8014b62abcbb00ae7
archana6832 <57583300+archana6832@users.noreply.github.com>
2021-12-17 16:30:44 +0100

    Update actions.js
    actions updated . Removed update user

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
-export const UPDATE_USER = 'UPDATE USER';
+
 
 export function setMovies(value) {
     console.log('Set movies action triggered')
@@ -22,14 +22,6 @@ export function setUser(value) {
     console.log('set_user action triggered')
     return {
         type: SET_USER,
-        value
-    };
-}
-
-export function updateUser(value) {
-    console.log('update_user action triggered')
-    return {
-        type: UPDATE_USER,
         value
     };
 }
